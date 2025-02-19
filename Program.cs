e785592adcdbaf4885cde727ab3a11b925a00958
Yoan-Daniel Malinov <ioiototm@gmail.com>
2025-02-19 17:03:31 +0000

    Add Serilog for enhanced logging functionality
    Updated `LUTE-Server.csproj` to include `Serilog.AspNetCore` and `Serilog.Sinks.File` for logging. Modified `Program.cs` to configure Serilog to log to console and file, added error handling during startup, and ensured proper logger closure. Updated `CreateHostBuilder` to utilize Serilog.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,39 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        try
+        {
+            Log.Information("Starting up the application");
+            CreateHostBuilder(args).Build().Run();
+        }
+        catch (Exception ex)
+        {
+
+            Log.Fatal(ex, "Application start-up failed");
+
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+
+
+
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .UseSerilog()
             .ConfigureAppConfiguration((hostingContext, config) =>
             {
                 var env = hostingContext.HostingEnvironment;
